13d638194daf152f33f0aff04b8a6b9415b20c5d
Henrry Adelfio Soto Toledo <108912157+HeadeOne@users.noreply.github.com>
2025-03-14 22:31:31 +0000

    se arreglo las imagenes, el css de las barras de porcentaje de los skills tambien se agrego la funcionalidad a al formulario de contacto

--- js/script.js
+++ js/script.js
@@ -29,3 +29,35 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const skills = document.querySelectorAll(".progress-bar");
+    skills.forEach(skill => {
+        const percentage = skill.getAttribute("data-percentage");
+        skill.style.width = percentage + "%";
+    });
+});
+
+document.getElementById("contactForm").addEventListener("submit",function(event){
+    event.preventDefault();
+    
+    //Captura de Los datos Integrados
+    const nombre = document.querySelector('input[name="nombre"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const telefono = document.querySelector('input[name="telefono"]').value;
+    const asunto = document.querySelector('input[name="asunto"]').value;
+    const mensaje = document.querySelector('textarea[name="mensaje"]').value;
+
+    // Simulación de envío (puedes mostrarlo en la consola o como alerta)
+    console.log("Formulario enviado con los siguientes datos: ");
+    console.log("Nombre:",nombre);
+    console.log("Email:",email);
+    console.log("Teléfono:", telefono);
+    console.log("Asunto:", asunto);
+    console.log("Mensaje:", mensaje);
+
+    alert("¡Mensaje enviado correctamente");
+
+    // Opcional: Resetear el formulario después del envío simulado
+    document.getElementById("contactForm").reset();
+});
